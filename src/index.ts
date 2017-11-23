ae4452bf7e641fa0c8a4d5de3cdf4c687ba0868c
Shahar Or (mightyiam) <mightyiampresence@gmail.com>
2017-11-23 20:06:25 +0200

    set no-unused-vars back to error

--- src/index.ts
+++ src/index.ts
@@ -25,10 +25,7 @@ export = {
     'typescript/no-angle-bracket-type-assertion': 'error',
     'typescript/no-namespace': 'error',
     'typescript/no-use-before-define': ['error', { functions: false, classes: false, variables: false, typedefs: false }],
-
-    'no-unused-vars': 'warn', // https://github.com/nzakas/eslint-plugin-typescript/pull/84
-    'typescript/no-unused-vars': 'warn',
-
+    'typescript/no-unused-vars': 'error',
     'typescript/adjacent-overload-signatures': 'error',
     'typescript/member-delimiter-style': ['error', { delimiter: 'none' }],
     'typescript/no-empty-interface': 'error'
