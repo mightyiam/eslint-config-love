71327fdd4f1a6194abce1076ba0de2143d1fb97c
Shahar Or (mightyiam) <mightyiampresence@gmail.com>
2018-04-08 11:34:13 +0300

    sort rules

--- src/index.ts
+++ src/index.ts
@@ -6,34 +6,26 @@ export = {
       jsx: false
     }
   },
-  plugins: [
-    'typescript'
-  ],
+  plugins: ['typescript'],
   rules: {
-    // https://github.com/nzakas/eslint-plugin-typescript/issues/85
-    'space-infix-ops': 'warn',
-
-    // https://github.com/eslint/typescript-eslint-parser/issues/77
+    'no-array-constructor': 'off', // in favor of TypeScript rule
     'no-undef': 'off', // TypeScript has this functionality by default
     'no-unused-vars': 'off', // TypeScript has `noUnusedLocals` and `noUnusedParameters`
-
     'no-useless-constructor': 'warn',
-    'typescript/type-annotation-spacing': 'error',
-    'typescript/explicit-member-accessibility': 'error',
-
-    'no-array-constructor': 'off', // in favor of TypeScript rule
-    'typescript/no-array-constructor': 'error',
-
+    'space-infix-ops': 'warn', // https://github.com/nzakas/eslint-plugin-typescript/issues/85
+    'typescript/adjacent-overload-signatures': 'error',
     'typescript/explicit-function-return-type': 'error',
-    'typescript/no-triple-slash-reference': 'error',
+    'typescript/explicit-member-accessibility': 'error',
+    'typescript/member-delimiter-style': ['error', { delimiter: 'none' }],
     'typescript/no-angle-bracket-type-assertion': 'error',
+    'typescript/no-array-constructor': 'error',
+    'typescript/no-empty-interface': 'error',
     'typescript/no-namespace': 'error',
     'typescript/no-non-null-assertion': 'error',
-    'typescript/no-use-before-define': ['error', { functions: false, classes: false, variables: false, typedefs: false }],
+    'typescript/no-triple-slash-reference': 'error',
     'typescript/no-unused-vars': 'error',
+    'typescript/no-use-before-define': ['error', { functions: false, classes: false, variables: false, typedefs: false }],
     'typescript/no-var-requires': 'error',
-    'typescript/adjacent-overload-signatures': 'error',
-    'typescript/member-delimiter-style': ['error', { delimiter: 'none' }],
-    'typescript/no-empty-interface': 'error'
+    'typescript/type-annotation-spacing': 'error'
   }
 }
