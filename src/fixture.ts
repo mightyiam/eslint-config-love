7918e91ccb233fd5b9e0a3b394f85c2cd4e35553
Shahar Or (mightyiam) <mightyiampresence@gmail.com>
2018-11-06 23:16:14 +0700

    chore: update all deps, update readme, add a fixture file
    BREAKING CHANGE: peer depends on `eslint@^5.8.0`
    
    Fixes #10

---
+++ src/fixture.ts
@@ -0,0 +1,21 @@
+/**
+ * https://github.com/mightyiam/eslint-config-standard-with-typescript/issues/10
+ */
+export type Bar = null
+export type Foo<Bar> = (a: Bar) => Bar
+
+/**
+ * https://github.com/mightyiam/eslint-config-standard-with-typescript/issues/2
+ */
+export default class Zoo {
+  public constructor (private name: string) {}
+
+  public get greeting (): string {
+    return `Hello ${this.name}`
+  }
+}
+
+/**
+ * https://github.com/mightyiam/eslint-config-standard-with-typescript/issues/3
+ */
+export interface Boo { b_oo: null }
