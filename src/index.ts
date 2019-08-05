6c8c04c3a8302df57c751156af24d59c3eca78a3
Shahar Or (mightyiam) <mightyiampresence@gmail.com>
2019-08-05 10:58:17 +0700

    feat: update ESLint & TypeScript plugin
    BREAKING CHANGE: `eslint-config-standard` has been updated to `13.0.1`.
    BREAKING CHANGE: Updatd all peer dependencies. Fixes #80. Fixes #94.
    
    Added option for rule `@typescript-eslint/explicit-function-return-type`: `allowHigherOrderFunctions: true`.
    
    BREAKING CHANGE: `typescript-eslint/parser` is no longer a `peerDependency`, but a `dependency`. Users should remove it from their configuration, as well. Closes #65.
    
    Updated `@typescript-eslint/eslint-plugin` to `1.10.2`.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 export = {
+  parser: '@typescript-eslint/parser',
   extends: 'eslint-config-standard',
   plugins: ['@typescript-eslint'],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
       rules: {
-        'camelcase': 'off', // in favor of TypeScript rule
-        'indent': 'off',
+        camelcase: 'off', // in favor of TypeScript rule
+        indent: 'off',
         'no-array-constructor': 'off',
         'no-undef': 'off', // TypeScript has this functionality by default
         'no-unused-vars': 'off', // in favor of TypeScript rule
@@ -14,21 +15,23 @@ export = {
         '@typescript-eslint/adjacent-overload-signatures': 'error',
         '@typescript-eslint/array-type': ['error', 'array-simple'],
         '@typescript-eslint/camelcase': ['error', { properties: 'never' }],
-        '@typescript-eslint/explicit-function-return-type': 'error',
+        '@typescript-eslint/explicit-function-return-type': ['error', {
+          allowHigherOrderFunctions: true
+        }],
         '@typescript-eslint/explicit-member-accessibility': 'error',
         '@typescript-eslint/indent': ['error', 2, {
-          'SwitchCase': 1,
-          'VariableDeclarator': 1,
-          'outerIIFEBody': 1,
-          'MemberExpression': 1,
-          'FunctionDeclaration': { 'parameters': 1, 'body': 1 },
-          'FunctionExpression': { 'parameters': 1, 'body': 1 },
-          'CallExpression': { 'arguments': 1 },
-          'ArrayExpression': 1,
-          'ObjectExpression': 1,
-          'ImportDeclaration': 1,
-          'flatTernaryExpressions': false,
-          'ignoreComments': false
+          SwitchCase: 1,
+          VariableDeclarator: 1,
+          outerIIFEBody: 1,
+          MemberExpression: 1,
+          FunctionDeclaration: { parameters: 1, body: 1 },
+          FunctionExpression: { parameters: 1, body: 1 },
+          CallExpression: { arguments: 1 },
+          ArrayExpression: 1,
+          ObjectExpression: 1,
+          ImportDeclaration: 1,
+          flatTernaryExpressions: false,
+          ignoreComments: false
         }],
         '@typescript-eslint/member-delimiter-style': [
           'error',
