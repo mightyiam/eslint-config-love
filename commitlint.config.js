d9ea4142a56da287faee94ad4c4b4f3812bf4e32
Shahar Or (mightyiam) <mightyiampresence@gmail.com>
2020-07-15 18:34:00 +0700

    dx(commitlint): crazy commits convention

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,11 @@
-module.exports = { extends: ['awesome'] }
+module.exports = {
+  extends: [require.resolve('crazy-commits/commitlint')],
+  rules: {
+    'scope-enum': [2, 'always', [
+      'commitlint',
+      'package',
+      'docs',
+      'deps'
+    ]]
+  }
+}
