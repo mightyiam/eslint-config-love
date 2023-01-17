94fcc736c0fac1b7aba743310cb5f1ee382d8d3f
Shahar Dawn Or <mightyiampresence@gmail.com>
2023-01-17 14:59:44 +0700

    feat: @typescript-eslint/ban-types
    BREAKING CHANGE: @typescript-eslint/ban-types

--- src/index.ts
+++ src/index.ts
@@ -81,6 +81,54 @@ const config: Linter.Config = {
           minimumDescriptionLength: 3
         }],
         '@typescript-eslint/ban-tslint-comment': 'error',
+        '@typescript-eslint/ban-types': ['error', {
+          extendDefaults: false,
+          types: {
+            String: {
+              message: 'Use string instead',
+              fixWith: 'string'
+            },
+            Boolean: {
+              message: 'Use boolean instead',
+              fixWith: 'boolean'
+            },
+            Number: {
+              message: 'Use number instead',
+              fixWith: 'number'
+            },
+            Symbol: {
+              message: 'Use symbol instead',
+              fixWith: 'symbol'
+            },
+            BigInt: {
+              message: 'Use bigint instead',
+              fixWith: 'bigint'
+            },
+            Function: {
+              message: [
+                'The `Function` type accepts any function-like value.',
+                'It provides no type safety when calling the function, which can be a common source of bugs.',
+                'It also accepts things like class declarations, which will throw at runtime as they will not be called with `new`.',
+                'If you are expecting the function to accept certain arguments, you should explicitly define the function shape.'
+              ].join('\n')
+            },
+            // object typing
+            Object: {
+              message: [
+                'The `Object` type actually means "any non-nullish value", so it is marginally better than `unknown`.',
+                '- If you want a type meaning "any object", you probably want `Record<string, unknown>` instead.',
+                '- If you want a type meaning "any value", you probably want `unknown` instead.'
+              ].join('\n')
+            },
+            '{}': {
+              message: [
+                '`{}` actually means "any non-nullish value".',
+                '- If you want a type meaning "any object", you probably want `Record<string, unknown>` instead.',
+                '- If you want a type meaning "any value", you probably want `unknown` instead.'
+              ].join('\n')
+            }
+          }
+        }],
         '@typescript-eslint/comma-dangle': ['error', {
           arrays: 'never',
           objects: 'never',
