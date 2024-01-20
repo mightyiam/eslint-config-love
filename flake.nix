afa0a5b767d9b1581cc0f6ae6fbc40dddba8bad4
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-01-20 15:38:20 +0700

    build: flake.nix with devShell

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    self,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = [pkgs.nodejs];
+      };
+    });
+}
