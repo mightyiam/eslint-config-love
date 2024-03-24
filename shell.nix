662a8f42d6321c25d366f9d1f92482bbd0cc959c
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-03-24 18:51:23 +0700

    build(shell.nix): typescript-language-server

--- shell.nix
+++ shell.nix
@@ -2,5 +2,8 @@ let
   pkgs = import <nixpkgs> {};
 in
   pkgs.mkShell {
-    packages = [pkgs.nodejs_latest];
+    packages = [
+      pkgs.nodejs_latest
+      pkgs.nodePackages_latest.typescript-language-server
+    ];
   }
