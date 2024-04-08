a392a637bad5638d1762c5aab134c0679a4d1e95
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-04-08 15:23:55 +0700

    chore: separate resolved config tests
    closes #1504

--- src/test/resolved-config.ts
+++ src/test/resolved-config.ts
@@ -3,17 +3,29 @@ import { ESLint } from 'eslint'
 import exported from '..'
 import { expectedExportedValue } from './_util'
 
-test('resolved config', async (t): Promise<void> => {
-  const eslint = new ESLint({
-    useEslintrc: false,
-    overrideConfig: structuredClone(exported)
-  })
+const eslint = new ESLint({
+  useEslintrc: false,
+  overrideConfig: structuredClone(exported)
+})
 
-  const actual = await eslint.calculateConfigForFile('foo.js')
+const actual = eslint.calculateConfigForFile('foo.js')
 
-  t.deepEqual(actual.env, expectedExportedValue.env, 'env')
-  t.deepEqual(actual.plugins, [...expectedExportedValue.plugins].reverse(), 'plugins')
-  t.deepEqual(actual.globals, expectedExportedValue.globals, 'globals')
-  t.true(actual.parser.includes(expectedExportedValue.parser), 'parser')
-  t.deepEqual(actual.rules, expectedExportedValue.rules, 'rules')
+test('env', async (t) => {
+  t.deepEqual((await actual).env, expectedExportedValue.env)
+})
+
+test('plugins', async (t) => {
+  t.deepEqual((await actual).plugins, [...expectedExportedValue.plugins].reverse())
+})
+
+test('globals', async (t) => {
+  t.deepEqual((await actual).globals, expectedExportedValue.globals)
+})
+
+test('parser', async (t) => {
+  t.true((await actual).parser.includes(expectedExportedValue.parser))
+})
+
+test('rules', async (t) => {
+  t.deepEqual((await actual).rules, expectedExportedValue.rules)
 })
