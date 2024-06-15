de9b499303a19810dccab65f850dfbb9c5a13e04
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-06-15 13:56:36 +0700

    test: no import sub-dep @typescript-eslint/eslint-plugin
    Co-authored-by: Rostislav Simonik <rostislav.simonik@technologystudio.sk>

--- src/test/equivalents.ts
+++ src/test/equivalents.ts
@@ -1,13 +1,14 @@
 import test from 'ava'
-import { rules as tseslintRules } from '@typescript-eslint/eslint-plugin'
+import { plugin } from 'typescript-eslint'
 import { equivalents, ourRules } from './_util'
 import { type TSESLint } from '@typescript-eslint/utils'
 
 test('JS equivalent rules are off', async (t) => {
+  if (plugin.rules === undefined) throw new Error()
   const ourRules_: TSESLint.FlatConfig.Rules = ourRules
   Object.keys(ourRules_).forEach((name) => {
     const bareName = name.replace('@typescript-eslint/', '')
-    if (!Object.prototype.hasOwnProperty.call(tseslintRules, bareName)) return
+    if (!Object.prototype.hasOwnProperty.call(plugin.rules, bareName)) return
     if (!equivalents.includes(bareName)) return
     const config = ourRules_[bareName]
     t.deepEqual(config, ['off'], bareName)
