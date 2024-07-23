9970ecf522b54a26fbf8f83a9c400dd93e9cd39b
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-07-23 19:50:26 +0700

    feat!: @typescript-eslint/no-import-type-side-effects

--- src/plugins.d.ts
+++ src/plugins.d.ts
@@ -1,15 +1,15 @@
 declare module 'eslint-plugin-n' {
-  import { type TSESLint } from '@typescript-eslint/utils'
+  import type { TSESLint } from '@typescript-eslint/utils'
   const plugin: TSESLint.Linter.Plugin
   export = plugin
 }
 declare module 'eslint-plugin-import' {
-  import { type TSESLint } from '@typescript-eslint/utils'
+  import type { TSESLint } from '@typescript-eslint/utils'
   const plugin: TSESLint.Linter.Plugin
   export = plugin
 }
 declare module 'eslint-plugin-promise' {
-  import { type TSESLint } from '@typescript-eslint/utils'
+  import type { TSESLint } from '@typescript-eslint/utils'
   const plugin: TSESLint.Linter.Plugin
   export = plugin
 }
