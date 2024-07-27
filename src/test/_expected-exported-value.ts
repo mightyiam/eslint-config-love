88579274cf7d3aad4800f166ae4135596d538c8b
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-07-27 14:22:59 +0700

    feat!: @typescript-eslint/no-inferrable-types

--- src/test/_expected-exported-value.ts
+++ src/test/_expected-exported-value.ts
@@ -336,6 +336,10 @@ export const expectedExportedValue: TSESLint.FlatConfig.Config = {
     '@typescript-eslint/no-for-in-array': ['error'],
     '@typescript-eslint/no-implied-eval': ['error'],
     '@typescript-eslint/no-import-type-side-effects': ['error'],
+    '@typescript-eslint/no-inferrable-types': [
+      'error',
+      { ignoreParameters: false, ignoreProperties: false },
+    ],
     '@typescript-eslint/no-invalid-void-type': ['error'],
     '@typescript-eslint/no-loop-func': ['error'],
     '@typescript-eslint/no-loss-of-precision': ['error'],
