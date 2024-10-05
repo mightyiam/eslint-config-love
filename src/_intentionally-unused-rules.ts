ae550954c046228a523cd44ec3e0a687653b583a
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-10-05 17:04:19 +0700

    chore: reorganize intentionally unused rule list

--- src/_intentionally-unused-rules.ts
+++ src/_intentionally-unused-rules.ts
@@ -1,24 +1,32 @@
 export const intentionallyUnusedRules: string[] = [
-  // From rule documentation:
-  // > If possible, it is recommended to use tsconfig's noImplicitReturns option rather than this rule. noImplicitReturns is powered by TS's type information and control-flow analysis so it has better coverage than this rule.
-  '@typescript-eslint/consistent-return',
-  'consistent-return',
+  // ## Incompatible with JS
 
-  // From trule documentation:
+  // From rule documentation:
   // > If you are working on a codebase within which you lint non-TypeScript code (i.e. `.js`/`.mjs`/`.cjs`/`.jsx`), you should ensure that you should use [ESLint `overrides`](https://eslint.org/docs/user-guide/configuring#disabling-rules-only-for-a-group-of-files) to only enable the rule on `.ts`/`.mts`/`.cts`/`.tsx` files. If you don't, then you will get unfixable lint errors reported within `.js`/`.mjs`/`.cjs`/`.jsx` files.
   '@typescript-eslint/explicit-member-accessibility',
+
+  // ## "too much"
 
   '@typescript-eslint/default-param-last',
   'default-param-last',
 
-  // style
   '@typescript-eslint/member-ordering',
 
-  // Made redundant by TS strict mode and `@typescript-eslint/explicit-function-return-type`
+  '@typescript-eslint/no-shadow',
+  'no-shadow',
+
+  // ## redundant
+
+  //  TS strict mode and `@typescript-eslint/explicit-function-return-type`
   '@typescript-eslint/explicit-module-boundary-types',
 
-  // Made redundant by no-new-native-nonconstructor
+  // no-new-native-nonconstructor
   'no-new-symbol',
+
+  // From rule documentation:
+  // > If possible, it is recommended to use tsconfig's noImplicitReturns option rather than this rule. noImplicitReturns is powered by TS's type information and control-flow analysis so it has better coverage than this rule.
+  '@typescript-eslint/consistent-return',
+  'consistent-return',
 
   // From rule documentation:
   // > Requiring type annotations unnecessarily can be cumbersome to maintain and generally reduces code readability. TypeScript is often better at inferring types than easily written type annotations would allow.
@@ -29,14 +37,15 @@ export const intentionallyUnusedRules: string[] = [
   // Covered by strict TypeScript
   '@typescript-eslint/no-invalid-this',
 
-  // For project specific usage
-  '@typescript-eslint/no-restricted-imports',
-  '@typescript-eslint/no-restricted-types',
-  'n/no-restricted-import',
-  'n/no-restricted-require',
-  'no-restricted-imports',
+  // ## Project specific
 
-  // Too strict
-  '@typescript-eslint/no-shadow',
-  'no-shadow',
+  '@typescript-eslint/no-restricted-imports',
+
+  '@typescript-eslint/no-restricted-types',
+
+  'n/no-restricted-import',
+
+  'n/no-restricted-require',
+
+  'no-restricted-imports',
 ]
