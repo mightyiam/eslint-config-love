d1ca57c6c91db7957f3e2e91235cb9938355b554
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2024-11-09 16:03:07 +0700

    refactor: use TSESLint.FlatConfig.Plugin

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,41 +1,41 @@
 declare module 'eslint-plugin-eslint-comments' {
   import type { TSESLint } from '@typescript-eslint/utils'
-  const plugin: TSESLint.Linter.Plugin
+  const plugin: TSESLint.FlatConfig.Plugin
   export default plugin
 }
 declare module 'eslint-plugin-eslint-comments_bottom' {
   import type { TSESLint } from '@typescript-eslint/utils'
-  const plugin: TSESLint.Linter.Plugin
+  const plugin: TSESLint.FlatConfig.Plugin
   export default plugin
 }
 declare module 'eslint-plugin-n' {
   import type { TSESLint } from '@typescript-eslint/utils'
-  const plugin: TSESLint.Linter.Plugin
+  const plugin: TSESLint.FlatConfig.Plugin
   export default plugin
 }
 declare module 'eslint-plugin-n_bottom' {
   import type { TSESLint } from '@typescript-eslint/utils'
-  const plugin: TSESLint.Linter.Plugin
+  const plugin: TSESLint.FlatConfig.Plugin
   export default plugin
 }
 declare module 'eslint-plugin-import' {
   import type { TSESLint } from '@typescript-eslint/utils'
-  const plugin: TSESLint.Linter.Plugin
+  const plugin: TSESLint.FlatConfig.Plugin
   export default plugin
 }
 declare module 'eslint-plugin-import_bottom' {
   import type { TSESLint } from '@typescript-eslint/utils'
-  const plugin: TSESLint.Linter.Plugin
+  const plugin: TSESLint.FlatConfig.Plugin
   export default plugin
 }
 declare module 'eslint-plugin-promise' {
   import type { TSESLint } from '@typescript-eslint/utils'
-  const plugin: TSESLint.Linter.Plugin
+  const plugin: TSESLint.FlatConfig.Plugin
   export default plugin
 }
 declare module 'eslint-plugin-promise_bottom' {
   import type { TSESLint } from '@typescript-eslint/utils'
-  const plugin: TSESLint.Linter.Plugin
+  const plugin: TSESLint.FlatConfig.Plugin
   export default plugin
 }
 declare module 'eslint_bottom' {
