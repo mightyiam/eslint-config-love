6b15605ba8898c0411a53ca1c5189fab6900eed3
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-09-11 12:28:18 +0700

    chore: intentionally no use no-ternary
    Co-authored-by: Saulo Nunes <saulo@paralink.com.br>

--- src/test/_intentionally-unused-rules.ts
+++ src/test/_intentionally-unused-rules.ts
@@ -8,6 +8,9 @@ export const intentionallyUnusedRules: string[] = [
   // > If you are working on a codebase within which you lint non-TypeScript code (i.e. `.js`/`.mjs`/`.cjs`/`.jsx`), you should ensure that you should use [ESLint `overrides`](https://eslint.org/docs/user-guide/configuring#disabling-rules-only-for-a-group-of-files) to only enable the rule on `.ts`/`.mts`/`.cts`/`.tsx` files. If you don't, then you will get unfixable lint errors reported within `.js`/`.mjs`/`.cjs`/`.jsx` files.
   '@typescript-eslint/explicit-member-accessibility',
 
+  // ## removes a desired language feature
+  'no-ternary',
+
   // ## "too much"
 
   'no-inline-comments',
